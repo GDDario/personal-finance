d91f0b4d0a59cf3fb8e7a5f90fc159432a2449b9
GDDario <gd.dario.dev@gmail.com>
2024-12-15 17:44:36 -0300

    chore: add a settings button to menu

--- lib/ui/core/ui/main_menu/widgets/main_menu.dart
+++ lib/ui/core/ui/main_menu/widgets/main_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_finance/ui/core/themes/default_metrics.dart';
 import 'package:provider/provider.dart';
 import 'package:personal_finance/ui/core/ui/main_menu/view_models/main_menu_viewmodel.dart';
 import 'package:personal_finance/ui/core/ui/main_menu/widgets/menu_items.dart';
@@ -56,8 +57,56 @@ class MainMenu extends StatelessWidget {
                 ),
                 const SizedBox(height: 40),
                 const Expanded(child: MenuItems()),
+                const Spacer(),
+                SettingsButton()
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final expanded = context.watch<MainMenuViewModel>().expanded;
+    final isSelected = false;
+    final textColor = isSelected
+        ? Theme.of(context).colorScheme.primary
+        : Theme.of(context).colorScheme.onPrimary;
+    final bgColor = isSelected
+        ? Theme.of(context).colorScheme.onPrimary
+        : Colors.transparent;
+
+    return Material(
+      shape: const RoundedRectangleBorder(
+        borderRadius: DefaultMetrics.roundedBorderRadius,
+      ),
+      color: bgColor,
+      child: InkWell(
+        borderRadius: DefaultMetrics.roundedBorderRadius,
+        onTap: () => () {},
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Icon(Icons.settings, color: textColor),
+              if (expanded)
+                Row(
+                  children: [
+                    const SizedBox(width: 15.0),
+                    Text(
+                      'Settings',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: textColor,
+                      ),
+                    ),
+                  ],
+                )
+            ],
           ),
         ),
       ),
