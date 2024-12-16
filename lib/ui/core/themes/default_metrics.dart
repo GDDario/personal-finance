60354c6fdda1db327114f8e7c1fbd818b7313dab
GDDario <gd.dario.dev@gmail.com>
2024-12-16 13:04:29 -0300

    chore: add input decoration theme

--- lib/ui/core/themes/default_metrics.dart
+++ lib/ui/core/themes/default_metrics.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 sealed class DefaultMetrics {
   static const double screenPadding = 20.0;
   static const double borderRadius = 8.0;
+  static const double fieldBorderRadius = 4.0;
 
   static const BorderRadius roundedBorderRadius = BorderRadius.all(
     Radius.circular(
--- lib/ui/core/themes/theme.dart
+++ lib/ui/core/themes/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_finance/ui/core/themes/colors.dart';
+import 'package:personal_finance/ui/core/themes/default_metrics.dart';
 
 sealed class AppTheme {
   static const _textTheme = TextTheme(
@@ -9,10 +10,19 @@ sealed class AppTheme {
     ),
   );
 
+  static final InputDecorationTheme _inputDecorationTheme =
+      InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderSide: const BorderSide(color: Colors.grey),
+      borderRadius: BorderRadius.circular(DefaultMetrics.fieldBorderRadius),
+    ),
+  );
+
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
     colorScheme: AppColors.lightColorScheme,
+    inputDecorationTheme: _inputDecorationTheme,
     textTheme: _textTheme,
     fontFamily: 'Inter',
   );
