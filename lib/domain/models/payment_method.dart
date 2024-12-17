24cd9f6133a0578cea7e14f83acc7b60a38246e9
GDDario <gd.dario.dev@gmail.com>
2024-12-17 12:41:53 -0300

    refactor: make all private fields public

--- lib/domain/models/payment_method.dart
+++ lib/domain/models/payment_method.dart
@@ -1,42 +1,10 @@
 class PaymentMethod {
-  late int _id;
-  late String _name;
-  late DateTime _createdAt;
-  late DateTime? _updatedAt;
+  late int id;
+  late String name;
+  late DateTime createdAt;
+  late DateTime? updatedAt;
 
-  PaymentMethod(int id, String name, DateTime createdAt, DateTime? updatedAt) {
-    _id = id;
-    _name = name;
-    _createdAt = createdAt;
-    _id = id;
-  }
+  PaymentMethod(this.id, this.name, this.createdAt, this.updatedAt);
 
-  PaymentMethod.simple(int id, String name) {
-    _id = id;
-    _name = name;
-  }
-
-  DateTime? get updatedAt => _updatedAt;
-
-  set updatedAt(DateTime? value) {
-    _updatedAt = value;
-  }
-
-  DateTime get createdAt => _createdAt;
-
-  set createdAt(DateTime value) {
-    _createdAt = value;
-  }
-
-  String get name => _name;
-
-  set name(String value) {
-    _name = value;
-  }
-
-  int get id => _id;
-
-  set id(int value) {
-    _id = value;
-  }
+  PaymentMethod.simple(this.id, this.name);
 }
