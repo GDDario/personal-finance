8f877e7edb056f451a345037e8bdadee74b91f05
GDDario <gd.dario.dev@gmail.com>
2024-12-24 17:15:34 -0300

    feat: establishment model and mock

---
+++ lib/data/mock/establishments_mock.dart
@@ -0,0 +1,9 @@
+import 'package:personal_finance/domain/models/establishment.dart';
+
+List<Establishment> establishmentsMock = [
+  Establishment(1, 'Local Market', 'A market located close to me'),
+  Establishment(2, 'Bakery', 'A good nice bakery'),
+  Establishment(3, 'Amazon', 'Online marketplace'),
+  Establishment(4, 'Aliexpress', null),
+  Establishment(5, 'Amazon', null),
+];
---
+++ lib/domain/models/establishment.dart
@@ -0,0 +1,7 @@
+class Establishment {
+  late int id;
+  late String name;
+  late String? description;
+
+  Establishment(this.id, this.name, this.description);
+}
